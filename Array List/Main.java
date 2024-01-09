be60a73954a02722fa048de11f9f08ed3afe7d47
arshedgithub <arshedahmedmail@gmail.com>
2024-01-09 15:09:02 +0530

    some  changes in main file

--- Array List/Main.java
+++ Array List/Main.java
@@ -4,11 +4,15 @@ public class Main {
         
         ArrayList list = new ArrayList();
 
-        list.add(10);         list.add(10);        list.add(10);
-       list.add(10);
-        list.add(10);
-
+        list.add(10);         
+        list.add(20);        
+        list.add(30);
+        
         System.out.println(list.isFull());
+
+        list.add(40);
+        list.add(50);
+
         list.printAll();
 
     }
