02bd58049c1c84d66c89c8e0c8386cef9906fcf7
arshedgithub <arshedahmedmail@gmail.com>
2024-01-14 06:21:03 +0530

    add pop function

--- Stack/Array Based/Stack.java
+++ Stack/Array Based/Stack.java
@@ -13,23 +13,50 @@ public class Stack {
     }
     
     public boolean isEmpty(){
-        // if (tos == -1) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        return tos == -1;
+        if (tos == -1) {
+            return true;
+        } else {
+            return false;
+        }
+        // return tos == -1;
     }
 
     public boolean isFull(){
-        // if (tos == data.length - 1) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        return tos == data.length - 1;
+        if (tos == data.length - 1) {
+            return true;
+        } else {
+            return false;
+        }
+        // return tos == data.length - 1;
     }
 
-    
+    public void push(int value){
+        if (isFull()) {
+            System.out.println("OverFlow Error - stack is full.");
+        } else {
+            tos++;
+            data[tos] = value;
+            // data[++tos] = value;
+
+        }
+    }
+
+    public int pop(){
+        if (isEmpty()) {
+            System.out.println("UnderFlow Error - stack is empty");
+            return -1;
+        } else {
+            int value = data[tos];
+            tos--;
+            return value;
+            // return data[tos--];
+        }
+    }
+
+    public int peak(){
+        
+    }
+
+    public void printAll(){}
 
 }
